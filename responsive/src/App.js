8409e101c3832561451a14c4833bd425e813b2d5
Harshith1301 <anjiboya@gmail.com>
2023-03-11 22:53:49 +0530

    response

---
+++ responsive/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+    <button className='btn btn-primary'>clik Me</button>  
+    </div>
+  );
+}
+
+export default App;
